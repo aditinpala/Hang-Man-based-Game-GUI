2aeb67f1a155aefbf62b12d79787dbe777c073ca
Aditi Pala <60480137+aditinpala@users.noreply.github.com>
2020-08-01 08:46:20 +0530

    Add files via upload

---
+++ Hangman.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+public class Hangman 
+{
+    public static void main(String[] args)
+    {
+        Menu obj = new Menu();
+        obj.addComponent();
+    }
+}
